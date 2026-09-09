79a76472057c422e4e29488c6eae2f4a4246d6cc
Pallab Biswas <153554497+PallabBiswas3@users.noreply.github.com>
2026-09-10 03:00:47 +0530

    Export TEP knowledge-guided reasoning tools

--- src/tsdiag/tools/__init__.py
+++ src/tsdiag/tools/__init__.py
@@ -31,6 +31,7 @@ from .monitoring import (
 )
 from .process import causal_graph_filter, fault_onset_timing, process_diagnosis, root_cause_rank
 from .process_enhanced import pre_post_shift_evidence, root_cause_rank_enhanced
+from .tep_reasoning import knowledge_guided_root_cause_decision, rank_tep_fault_catalog
 from .change import change_point_detection, cross_sensor_relationships, rolling_statistics
 from .wavelet import (
     correlation_sensor_weighting,
@@ -51,6 +52,7 @@ __all__ = [
     "calibrated_pca_monitoring", "dpca_monitoring", "make_lagged_matrix",
     "run_monitoring_method", "causal_graph_filter", "fault_onset_timing",
     "root_cause_rank", "root_cause_rank_enhanced", "pre_post_shift_evidence",
+    "rank_tep_fault_catalog", "knowledge_guided_root_cause_decision",
     "process_diagnosis", "rolling_statistics", "cross_sensor_relationships",
     "change_point_detection", "wavelet_denoising", "cross_correlation_analysis",
     "correlation_sensor_weighting", "multisensor_fusion", "default_tool_registry",
